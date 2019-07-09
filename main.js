0abd41ec8773150f50689b7ebe589087975f001e
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-09 13:43:42 +0200

    Spostata verifica vittoria in una funzione, aggiunto controllo 84esimo input = vittoria

--- main.js
+++ main.js
@@ -3,30 +3,16 @@
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
-var gameOver = false, input, counter = 0;
+var mine, result;
 
 // genero 16 mine
-var mine = mineGenerator();
+mine = mineGenerator();
+// faccio inserire il numero dall'utente, guardo se è una mina, ritorno il punteggio
+result = gameChecker();
+// stampo il punteggio
+if (result === "won")	alert('CLAMOROSO! Hai vinto!');
+else					alert('Partita finita! Punteggio: ' + result);
 
-
-while (gameOver === false) {
-	// input giocatore
-	input = parseInt(prompt('Inserisci un numero:'));
-	console.log('inserito ' + input);
-	// verifico se è tra le mine
-	for (var i = 0; i < mine.length; i++) {
-		// se si partita finita
-		if (mine[i] === input) {
-			counter--;
-			gameOver = true;
-		}
-	}
-	// se no aumenta punteggio e richiedi input	
-	counter++;	
-}
-
-// a partita finita stampa punteggio
-alert('Partita finita! Punteggio: ' + counter);
 
 
 
@@ -34,4 +20,28 @@ alert('Partita finita! Punteggio: ' + counter);
 function mineGenerator() {
 	var array = [0,1,2,3,5,7,11,13,17,19,23,29,31,37,41,43];
 	return array;
+}
+
+
+function gameChecker() {
+	var gameOver = false, counter = 0, input;
+
+	while (gameOver === false) {
+
+		// controllo che non abbia già inserito tutti i numeri possibili
+		if (counter === 84) {
+			return "won";
+		}
+
+		input = parseInt(prompt('Inserisci un numero:'));
+		console.log('inserito ' + input);
+
+		// verifico se è tra le mine
+		for (var i = 0; i < mine.length; i++) {
+			// se si partita finita
+			if (mine[i] === input) 		return counter;
+		}
+		// se no aumenta punteggio
+		counter++;
+	}
 }
