e9bb8ff6b0681795d9285e33ee4507dde1471e75
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-09 18:09:00 +0200

    altVersion - aggiunto selettore difficoltà

--- altversion/main.js
+++ altversion/main.js
@@ -3,32 +3,51 @@
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
-var mine, result;
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali.
+// Con difficoltà 0=> da 1 a 100, con difficoltà 1 => da 1 a 80 con difficoltà 2=> da 1 a 50
 
+var mine, result, difficulty, mineQuantity, maxCorrectNumers;
+
+// chiedo la difficoltà
+difficulty = prompt('Inserisci la difficoltà, F (facile), M (medio) o D (difficile)');
+difficulty = difficulty.toLowerCase();
+if (difficulty == 'f')			difficulty = 100;
+else if (difficulty == 'm')		difficulty = 80;
+else if (difficulty == 'd')		difficulty = 50;
+else							alert('Uff, non gioco più con te :(');
+console.log('Difficoltà: ' + difficulty);
 
 // genero 16 mine
-var mineQuantity = 16;
-mine = mineGenerator(mineQuantity);
+mineQuantity = 16;
+mine = mineGenerator(mineQuantity, difficulty);
+
 // faccio inserire il numero dall'utente, guardo se è una mina, ritorno il punteggio
-result = gameChecker();
+result = gameChecker(mineQuantity, difficulty);
+
 // stampo il punteggio
-if (result === "won")	alert('CLAMOROSO! Hai vinto!');
-else					alert('Partita finita! Punteggio: ' + result);
+if (result === "won") {
+	console.log('CLAMOROSO! Hai vinto!');
+	alert('CLAMOROSO! Hai vinto!');
+} else {
+	console.log('Partita finita! Punteggio: ' + result);
+	alert('Partita finita! Punteggio: ' + result);
+}
+
 
 
 
 
 // # # FUNZIONI # #
-function mineGenerator(mineQuantity) {
+function mineGenerator(mineQuantity, maxValue) {
 	var numGenerated, array = [];
 
 	for (var i = 1; i < mineQuantity; i++) {
 		//genero un numero
-		numGenerated = Math.floor(Math.random() * 100 + 1);
+		numGenerated = Math.floor(Math.random() * maxValue + 1);
 		//controllo che non sia già stato generato in precedenza
 		for (var i = 0; i < array.length; i++) {
 				if (array[i] === numGenerated) {
-					numGenerated = Math.floor(Math.random() * 100 + 1);
+					numGenerated = Math.floor(Math.random() * maxValue + 1);
 					// ricontrollo da capo per evitare che il numero nuovo sia uguale a uno precedente
 					i = -1;
 					console.log(numGenerated + ' doppione, genero nuovo numero e riparto a controllare da capo');
@@ -37,39 +56,43 @@ function mineGenerator(mineQuantity) {
 		//pusho il numero generato (sicuramente non doppio) nell'array
 		array.push(numGenerated)
 	}
-	//ordino per comodità, non necessario
+	//ordino per comodità
 	array = array.sort();
 	console.log('mine generate: ' + array);
 	return array;
 }
 
 
-function gameChecker() {
-	var counter = 0, input, inputArray = [], correctInput = false, duplicate = false;
 
-	while (counter < 84) {
+function gameChecker(mineQuantity, difficulty) {
+	var counter = 0, input, inputArray = [], correctInput = false, duplicate = false;
+	var maxCorrectNumers = difficulty - mineQuantity;
+
+	// faccio inserire solo se non ha già inserito tutti i numeri inseribili (sarà..)
+	while (counter < maxCorrectNumers) {
 		input = parseInt(prompt('Inserisci un numero:'));
 
+		// controlli all'input iserito
 		while (correctInput === false) {
 			// accendo una flag se l'utente aveva già inserito quel numero
 			for (var i = 0; i < inputArray.length; i++) {
-				if (inputArray[i] === input) 		duplicate = true;
+				if (inputArray[i] === input)		duplicate = true;
 			}
-
-			// controllo che l'utente abbia inserito soltanto un numero
-			if (isNaN(input)) {
-				input = parseInt(prompt('Inserisci un numero:'));
-			}
-			// blocco intervallo 1 - 100
-			else if (input > 100 || input < 1) {
-				input = parseInt(prompt('Inserisci un numero da 1 a 100:'));
-			}
-			// faccio ri-inserire l'input se prima l'ho trovato doppio
-			else if (duplicate === true) {
+			// adesso gli faccio rifare
+			if (duplicate === true) {
 				duplicate = false;
 				console.log(input + " l'avevi già messo");
 				input = parseInt(prompt('Avevi già inserito quel numero, riprova:'));
 			}
+			// controllo che l'utente abbia inserito soltanto un numero
+			else if (isNaN(input)) {
+				input = parseInt(prompt('Inserisci un numero:'));
+			}
+			// blocco intervallo input da 1 a quanto consente la difficoltà scelta all'inizio
+			else if (input > difficulty || input < 1) {
+				input = parseInt(prompt('Inserisci un numero da 1 a ' + difficulty + ':'));
+			}
+			// tutto ok, usciamo dal while di controllo
 			else {
 				correctInput = true;
 			}
@@ -78,7 +101,7 @@ function gameChecker() {
 		console.log('inserito ' + input);
 		inputArray.push(input);
 
-		// verifico se è tra le mine
+		// verifico se l'input è tra le mine
 		for (var i = 0; i < mine.length; i++) {
 			// se si partita finita
 			if (mine[i] === input) 		return counter;
@@ -86,6 +109,6 @@ function gameChecker() {
 		// se no aumenta punteggio
 		counter++;
 	}
-	// se ha inserito tutti i numeri possibili NON mine ha vinto
+	// return solo se hai inserito tutti i valori che potevi mettere
 	return "won";
 }
