5b65b31dc20fd31868b2ffe9ad47247526cc53fc
Matteo Andreoli <maceo.andreoli@gmail.com>
2019-07-09 13:16:40 +0200

    Init

---
+++ main.js
@@ -0,0 +1,19 @@
+// Il computer deve generare 16 numeri casuali da 1 a 100.
+// In seguito deve chiedere all’utente di inserire un numero da 1 a 100 alla volta, se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+
+
+// genero 16 mine
+
+
+// input giocatore
+
+
+// verifico se è tra le mine
+	// se si partita finita
+	// se no aumenta punteggio e richiedi input
+
+
+// a partita finita stampa punteggio
